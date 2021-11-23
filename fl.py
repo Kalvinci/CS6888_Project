574fdc5d257873f963bee8a9eadd46ae2c4ba992
Kalvinci <kalyanasundharam97@gmail.com>
2021-11-23 13:18:25 -0500

    refactoring

--- fl.py
+++ fl.py
@@ -1,11 +1,12 @@
 import queryparser as qparser
 from replacement import getTruePositiveDocs, getTrueNegativeDoc
-from pymongo import MongoClient
+from pymongo import MongoClient, collection
 
 # "mongodb+srv://kalyan:k4ly4nk4l1@cluster0.ersqz.mongodb.net/test"
 client = MongoClient(port=27017)
-db = client["testdb"]
 
+db = client["testdb"]
+orig_collection = db["order"]
 primary_key = "Orderid"
 
 oracle_map = {}
@@ -14,17 +15,17 @@ result_map = {}
 wrong_query = {"$or": [{"$and": [{"Year": {"$gt": 2007}}, {"Price": {"$gt": 100}}]}, {"$and": [{"Zipcode": "10008"}, {"Discount": 0}]}]}
 correct_query = {"$or": [{"$and": [{"Year": {"$gt": 2009}}, {"Price": {"$gt": 100}}]}, {"$and": [{"Zipcode": "10007"}, {"Discount": 0}]}]}
 
-for document in db.order.find(correct_query):
+for document in orig_collection.find(correct_query):
 	oracle_map[document[primary_key]] = document
 
-for document in db.order.find(wrong_query):
+for document in orig_collection.find(wrong_query):
 	result_map[document[primary_key]] = document
 
-oracle_orderIds = set(oracle_map.keys())
-result_orderIds = set(result_map.keys())
+oracle_Ids = set(oracle_map.keys())
+result_Ids = set(result_map.keys())
 
-superflous = result_orderIds.difference(oracle_orderIds)
-absent = oracle_orderIds.difference(result_orderIds)
+superflous = result_Ids.difference(oracle_Ids)
+absent = oracle_Ids.difference(result_Ids)
 
 cp_clause_list, clause_list = qparser.parse(wrong_query)
 
@@ -40,7 +41,7 @@ for item in cp_clause_list:
 	ids = set()
 	cp = item["cp"]
 	clauses = item["clauses"]
-	for document in db.order.find(cp):
+	for document in orig_collection.find(cp):
 		ids.add(document[primary_key])
 	
 	for id in superflous.intersection(ids):
@@ -62,7 +63,7 @@ for item in cp_clause_list:
 	for id in absent.difference(ids):
 		for clause in clauses:
 			docids = set()
-			for doc in db.order.find(clause):
+			for doc in orig_collection.find(clause):
 				docids.add(doc[primary_key])
 			if id not in docids:
 				field = list(clause.keys())[0]
@@ -85,16 +86,16 @@ print("\nExonerating innocent clauses...")
 
 mut_collection = db["mutation"]
 
-truePos = oracle_orderIds.intersection(result_orderIds)
+truePos = oracle_Ids.intersection(result_Ids)
 
-replacement_docs = getTruePositiveDocs(db.order, cps, truePos, primary_key)
+replacement_docs = getTruePositiveDocs(orig_collection, cps, truePos, primary_key)
 
 for replacement_doc in replacement_docs:
-	for doc in db.order.find({primary_key: {"$in": list(superflous)}}):
+	for doc in orig_collection.find({primary_key: {"$in": list(superflous)}}):
 		id = doc[primary_key]
 		for field in superflous_clausemap[id].keys():
 			temp = doc[field]
-			doc[field] = replacement_doc[field]
+			doc[field] = replacement_doc[field] if field in replacement_doc else None
 			mut_collection.insert_one(doc)
 			ret = set()
 			for res_row in mut_collection.find(correct_query):
@@ -105,14 +106,14 @@ for replacement_doc in replacement_docs:
 			doc[field] = temp
 			mut_collection.delete_one({primary_key: id})
 
-replacement_doc = getTrueNegativeDoc(db.order, clause_list)
+replacement_doc = getTrueNegativeDoc(orig_collection, clause_list)
 
 if replacement_doc != None:
-	for doc in db.order.find({primary_key: {"$in": list(absent)}}):
+	for doc in orig_collection.find({primary_key: {"$in": list(absent)}}):
 		id = doc[primary_key]
 		for field in absent_clausemap[id].keys():
 			temp = doc[field]
-			doc[field] = replacement_doc[field]
+			doc[field] = replacement_doc[field] if field in replacement_doc else None
 			mut_collection.insert_one(doc)
 			ret = set()
 			for res_row in mut_collection.find(correct_query):
